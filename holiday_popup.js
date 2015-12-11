88a25893d4ad839cdc33ded99484fe2af3a4416f
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2015-12-11 13:07:56 -0500

    Southcentre 0.1  /holiday_popup.js

--- holiday_popup.js
+++ holiday_popup.js
@@ -24,7 +24,7 @@
      
             if (visited <= 1) {
                 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || winwidth <= 600) {
-                    //$(".hidden-popup-bg").show();
+                    $(".hidden-popup-bg").show();
                 }else{
                      $(".hidden-popup-bg").show();
                 }
